c283be357da2e1b6a0efa49ee1b979a7572bcef8
Christopher Cichielo <chris.cichielo@gmail.com>
2023-11-03 19:26:40 -0400

    init

---
+++ WaterBoy/WaterBoy Watch App/WaterBoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterBoyApp.swift
+//  WaterBoy Watch App
+//
+//  Created by Christopher Cichielo on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WaterBoy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
